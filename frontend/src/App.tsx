4d6a37adcd377a1c40de78c666025c1f3b5952bf
Aman Srivastava <aman.srivastava@ticketmaster.com>
2022-10-09 18:09:37 +0530

    Header layout implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import './styles/app.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
