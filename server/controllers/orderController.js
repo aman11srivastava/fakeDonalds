6654fe14964e3b62b4469d32f669924d22917573
Aman Srivastava <aman.srivastava@ticketmaster.com>
2022-11-26 20:19:15 +0530

    Online payment API with Razorpay

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,6 +2,9 @@ import { catchAsyncError } from '../middlewares/errorMiddleware.js';
 import { Order } from '../models/Order.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import { orderStatusEnum } from '../utils/utils.js';
+import crypto from 'crypto';
+import { Payment } from '../models/Payment.js';
+import { razorpayInstance } from '../server.js';
 
 export const createOrder = catchAsyncError(async (req, res, next) => {
     const {
@@ -14,7 +17,7 @@ export const createOrder = catchAsyncError(async (req, res, next) => {
         totalAmount,
     } = req.body;
 
-    const user = "req.user._id";
+    const user = req.user?._id;
 
     const orderOptions = {
         shippingInfo,
@@ -34,6 +37,66 @@ export const createOrder = catchAsyncError(async (req, res, next) => {
         message: "Order Placed Successfully via Cash On Delivery",
     });
 });
+
+export const createOnlineOrder = catchAsyncError(async (req, res, next) => {
+    const {
+        shippingInfo,
+        orderItems,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingCharges,
+        totalAmount,
+    } = req.body;
+
+    const user = req.user?._id;
+
+    const orderOptions = {
+        shippingInfo,
+        orderItems,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingCharges,
+        totalAmount,
+        user,
+    };
+
+    const paymentOptions = {
+        amount: Number(totalAmount) * 100,
+        currency: "INR",
+    };
+    const order = await razorpayInstance.orders.create(paymentOptions);
+    res.status(200).json({
+        success: true,
+        order,
+        orderOptions
+    })
+})
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+    const { razorpay_payment_id, razorpay_order_id, razorpay_signature, orderOptions } = req.body;
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_API_SECRET).update(body).digest("hex");
+    const isSignAuthentic = razorpay_signature === expectedSignature;
+    if (isSignAuthentic) {
+        const payment = await Payment.create({
+            razorpay_order_id, razorpay_payment_id, razorpay_signature
+        })
+        await Order.create({
+            ...orderOptions,
+            paidAt: new Date(Date.now()),
+            paymentInfo: payment?._id
+        })
+        res.status(201).json({
+            success: true,
+            message: `Order placed succefully. Payment Id: ${payment?._id}`
+        })
+    }
+    else {
+        return next(new ErrorHandler("Payment Failed", 400));
+    }
+})
 
 export const getMyOrders = catchAsyncError(async (req, res, next) => {
     const orders = await Order.find({
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createOrder, getAdminOrders, getMyOrders, getOrderDetails, processOrder } from '../controllers/orderController.js';
+import { createOnlineOrder, createOrder, getAdminOrders, getMyOrders, getOrderDetails, paymentVerification, processOrder } from '../controllers/orderController.js';
 import { authorizeAdmin, isAuthenticated } from '../middlewares/auth.js'
 
 const router = express.Router();
 
-router.route("/createOrder").post(createOrder)
+router.route("/createOrder").post(isAuthenticated, createOrder)
+router.route("/createOnlineOrder").post(isAuthenticated, createOnlineOrder)
+router.route("/paymentVerification").post(isAuthenticated, paymentVerification)
 router.route("/myorders").get(isAuthenticated, getMyOrders)
 router.route("/order/:id").get(isAuthenticated, getOrderDetails)
 router.route("/admin/orders").get(isAuthenticated, authorizeAdmin, getAdminOrders)
