f10dc1e55847aa4f9ae0d57a3bd9ee18e455dfb3
Aman Srivastava <aman.srivastava@ticketmaster.com>
2022-11-27 01:19:27 +0530

    BE deployement on heroku changes

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js'
+import cors from 'cors'
 
 const app = express();
 
@@ -17,7 +18,12 @@ dotenv.config({
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        httpOnly: process.env.NODE_ENV === "development" ? false : true,
+        sameSite: process.env.NODE_ENV === "development" ? false : "none"
+    }
 }))
 
 // middlewares
@@ -27,9 +33,16 @@ app.use(urlencoded({
     extended: true
 }));
 
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
+
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
 app.use(passport.session());
+app.enable("trust proxy");
 
 connectPassport();
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,16 +9,20 @@ router.get('/googlelogin', passport.authenticate("google", {
     scope: ["profile"]
 }))
 
-router.get('/login', 
-passport.authenticate("google"),
+// router.get('/login', 
+// passport.authenticate("google"),
 // {
 //     scope: ['profile'],
 //     successRedirect: process.env.FRONTEND_URL
 // })
-    (req, res, next) => {
-        res.send("Login Successful")
-    }
-)
+// //     // (req, res, next) => {
+// //     //     res.send("Login Successful")
+// //     // }
+// // )
+
+router.get("/login", passport.authenticate("google", {
+    successRedirect: process.env.FRONTEND_URL
+}))
 
 router.get("/me", isAuthenticated, myProfile);
 router.get("/logout", logout)
