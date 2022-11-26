982074e685e26903feac8558e3a5c280e74db886
Aman Srivastava <aman.srivastava@ticketmaster.com>
2022-11-26 17:39:02 +0530

    Order routes

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import { catchAsyncError } from '../middlewares/errorMiddleware.js';
 import { Order } from '../models/Order.js';
+import ErrorHandler from '../utils/errorHandler.js';
 
 export const createOrder = catchAsyncError(async (req, res, next) => {
     const {
@@ -31,4 +32,33 @@ export const createOrder = catchAsyncError(async (req, res, next) => {
         success: true,
         message: "Order Placed Successfully via Cash On Delivery",
     });
+});
+
+export const getMyOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({
+        user: req?.user?._id
+    }).populate("user", "name")
+    res.status(200).json({
+        success: true,
+        orders
+    })
+});
+
+export const getOrderDetails = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate("user", "name")
+    if (!order) {
+        return next(new ErrorHandler("Invalid Order Id", 404))
+    }
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+export const getAdminOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({}).populate("user", "name")
+    res.status(200).json({
+        success: true,
+        orders
+    })
 });
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createOrder } from '../controllers/orderController.js';
+import { createOrder, getAdminOrders, getMyOrders, getOrderDetails } from '../controllers/orderController.js';
 import { isAuthenticated } from '../middlewares/auth.js'
 
 const router = express.Router();
 
-router.route("/createOrder").post( createOrder)
+router.route("/createOrder").post(createOrder)
+router.route("/myorders").get(isAuthenticated, getMyOrders)
+router.route("/order/:id").get(isAuthenticated, getOrderDetails)
+// Todo => Add Admin middleware
+router.route("/admin/orders").get(isAuthenticated, getAdminOrders)
 
 export default router;
